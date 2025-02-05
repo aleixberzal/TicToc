9e57e47525e36bc00e9196edcc022a52e5e3e9e3
aleixberzal <aleix.berzal@enti.cat>
2025-02-05 23:47:26 +0100

    USANDO TERNARIOS CPU inicial creada. Viendo como hacer la lógica para crear la IA, he encontrado la manera de eliminar muchísima parte del código.

--- TicTocProject/Source.cpp
+++ TicTocProject/Source.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
 #include "gameplay.h"
-
+#include <cstdlib>
 #define xLength 7
 #define yLength 13
 
 void gamePlay(char tablero[xLength][yLength], int row, int column, bool turno1) {
+
+    /*Creamos la variable ficha para asignar una en cada turno correspondiente con el ternario*/
+
     char ficha = turno1 ? 'X' : 'O';
+    /*Usamos las operaciones matemáticas para acceder a la memoria de la array introduciendo solo 1, 2 y 3
+    1.- fila 1 * 2 - 1 es la fila 1
+    2.- fila 2 * 2 - 1 es la fila 2
+    3. - fila 3 * 2 - 1 es la fila 1 
+
+    Porque las posiciones para acceder a cada fila son en realidad la 1, la 3 y la 5.
+        */
     int tableroRow = row * 2 - 1;
+    
+    /*Usamos las operaciones matemáticas para acceder a la memoria de la array introduciendo solo 1, 2 y 3
+    
+1.- fila 1 * 4 - 2 es la fila 1
+2.- fila 2 * 4 - 2 es la fila 2
+3. - fila 3 * 4 - 2 es la fila 1
+
+Porque las posiciones para acceder a cada fila son en realidad la 2, la 6 y la 10.
+    */
+
     int tableroColumn = column * 4 - 2;
 
     if (tablero[tableroRow][tableroColumn] == ' ') {
+
         tablero[tableroRow][tableroColumn] = ficha;
+
     }
 }
 
 void createTable(char tablero[xLength][yLength]) {
+
     for (int i = 0; i < xLength; i++) {
         for (int j = 0; j < yLength; j++) {
+
+            /*Si no es par me imprime los pipes, solo en las posiciones multiples de 4, para que lo 
+            haga solo cada 4 posiciones y quede bien centrado*/
+
             if (i % 2 != 0) {
                 tablero[i][j] = (j % 4 == 0) ? '|' : ' ';
             }
@@ -28,6 +55,7 @@ void createTable(char tablero[xLength][yLength]) {
 }
 
 void printTable(char tablero[xLength][yLength]) {
+
     for (int i = 0; i < xLength; i++) {
         for (int j = 0; j < yLength; j++) {
             std::cout << tablero[i][j];
@@ -37,11 +65,15 @@ void printTable(char tablero[xLength][yLength]) {
 }
 
 void updateTable(char tablero[xLength][yLength], int row, int column, bool turno1) {
+
     gamePlay(tablero, row, column, turno1);
     printTable(tablero);
+
 }
 
 int main() {
+
+    srand(time(0));
     int row, column;
     char tablero[xLength][yLength];
     bool turno1 = true;
@@ -49,23 +81,41 @@ int main() {
     createTable(tablero);
     printTable(tablero);
 
-    while (true) { // Se repite el juego hasta que se decida detenerlo manualmente
+    while (true) { 
+
+        /*Si es el turno 1 saldrá el primer título, en caso de que no lo sea, saldrá el de después del doble punto*/
+
         std::cout << (turno1 ? "TURNO DEL JUGADOR 1" : "TURNO DEL JUGADOR 2") << std::endl;
 
-        do {
-            std::cout << "Choose a row (1-3): ";
-            std::cin >> row;
-            std::cout << "Choose a column (1-3): ";
-            std::cin >> column;
+        if (!turno1) {
 
-            if (row < 1 || row > 3 || column < 1 || column > 3) {
-                std::cout << "Posici\xF3n no v\xE1lida, vuelve a intentarlo.\n";
-            }
-        } while (row < 1 || row > 3 || column < 1 || column > 3); // Repite hasta que el movimiento sea válido
+            do {
+                row = rand() % 3 + 1;
+                column = rand() % 3 + 1;
+            } while (tablero[row * 2 - 1][column * 4 - 2] != ' ');
 
-        updateTable(tablero, row, column, turno1);
-        turno1 = !turno1; // Cambia de turno
+            std::cout << "I choose the row: " << row << std::endl;
+            std::cout << "I choose the column: " << column << std::endl;
+
+            updateTable(tablero, row, column, turno1);
+            turno1 = !turno1;
+        }
+        else {
+            do {
+                std::cout << "Choose a row (1-3): ";
+                std::cin >> row;
+                std::cout << "Choose a column (1-3): ";
+                std::cin >> column;
+
+                if (row < 1 || row > 3 || column < 1 || column > 3 || tablero[row * 2 - 1][column * 4 - 2] != ' ') {
+                    std::cout << "Posicion no valida, vuelve a intentarlo.\n";
+                }
+            } while (row < 1 || row > 3 || column < 1 || column > 3 || tablero[row * 2 - 1][column * 4 - 2] != ' ');
+
+            updateTable(tablero, row, column, turno1);
+            turno1 = !turno1; 
+        }
     }
 
-    return 0;
+
 }
